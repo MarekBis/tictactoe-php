fbd5db7f6efa326e939dc2e2830b21a2ba3e7070
MarekBis <marek.bis5@seznam.cz>
2024-02-21 22:18:43 +0100

    first page frontend done

--- index.php
+++ index.php
@@ -7,28 +7,33 @@
     <link rel="icon" href="./faviconGame.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TicTacToe Game</title>
 </head>
 <body>
     <header>
             <h1>tic tac toe</h1>
-            <h2>Please enter player names</h2>
+            <h2>select players</h2>
     </header>
     <section>
-        <div class="container">
-            <form action="" method="post">
-                <label for="" >Player 1</label>
-                <input type="text" name="player1-name" placeholder="Player 1">
-                <label for="" >Player 2</label>
-                <input type="text" name="player2-name" placeholder="Player 2">
-                <input type="submit" value="Play">
-            </form>
+        <div class="linearGradient">
+
+            <div class="container">
+                <form action="" method="post">
+                    <label for="player1-name" >Player 1</label>
+                    <input type="text" id="player1-name" name="player1-name">
+                    <label for="player2-name" >Player 2</label>
+                    <input type="text" id="player2-name" name="player2-name">
+                    <input class="button-30" type="submit" value="Play">
+                </form>
+            </div>
         </div>
     </section>
     <footer>
-
+        <strong>
+            <p>&copy;2024 Marek Bis</p>
+        </strong>
     </footer>
 </body>
 </html>
