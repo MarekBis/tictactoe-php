f2781cc098c2537fd1ac576f2b2aa8f7275b6ff4
MarekBis <marek.bis5@seznam.cz>
2024-02-21 19:24:10 +0100

    fonts,icons,favicon added

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="./faviconGame.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TicTacToe Game</title>
+</head>
+<body>
+    <header>
+            <h1>tic tac toe</h1>
+            <h2>Please enter player names</h2>
+    </header>
+    <section>
+        <div class="container">
+            <form action="" method="post">
+                <label for="" >Player 1</label>
+                <input type="text" name="player1-name" placeholder="Player 1">
+                <label for="" >Player 2</label>
+                <input type="text" name="player2-name" placeholder="Player 2">
+                <input type="submit" value="Play">
+            </form>
+        </div>
+    </section>
+    <footer>
+
+    </footer>
+</body>
+</html>
