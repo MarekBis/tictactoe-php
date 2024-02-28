445945c728e49b01ede01e9710926f4a3b917315
MarekBis <marek.bis5@seznam.cz>
2024-02-28 18:17:31 +0100

    WORKING GAME

--- code.php
+++ code.php
@@ -1,19 +1,9 @@
 <?php
 session_start();
-
-function sudeKolo($argPocetKol){
-    if ($argPocetKol % 2 == 0 || $argPocetKol == 0){
-        return true;
-    }else{
-        return false;
-    }
-
-}
-
-
-$znak1 = "<i class='fa-solid fa-o'></i>";
-$znak2 = "<i class='fa-solid fa-x'></i>";
-
+// nastaveni znaku
+$znak1 = "<i class='fa-solid fa-x'></i>";
+$znak2 = "<i class='fa-solid fa-o'></i>";
+// nastaveni session promennych pri poslani POSTU na prvni strance
 if (array_key_exists("players-submit",$_POST)){
     $hrac1 = $_POST["player1-name"];
     $hrac2 = $_POST["player2-name"];
@@ -30,12 +20,9 @@ if (array_key_exists("players-submit",$_POST)){
     $_SESSION["row3-col1"] = "";
     $_SESSION["row3-col2"] = "";
     $_SESSION["row3-col3"] = "";
+    $idStranky="playScene";
 
 }
-if (!array_key_exists("PHPSESSID",$_COOKIE)){
-    $idStranky = "startScene";
-}
-
 if (array_key_exists("pocetKol", $_SESSION)) {
     //existuje
     $_SESSION["pocetKol"]++;
@@ -43,7 +30,68 @@ if (array_key_exists("pocetKol", $_SESSION)) {
     $idStranky = "playScene";
 } else {
     //sess neexistuje
+    $idStranky = "startScene";
     $_SESSION["pocetKol"] = -1;
     $pocetKol = $_SESSION["pocetKol"];
 }
+// liche/sude kolo
+function vyber($pocetKol,$arg1,$arg2){
+    if ($pocetKol % 2 == 0 || $pocetKol == 0){
+        return $arg1;
+    }else{
+        return $arg2;
+    }
+}
+// zpracovani GET formulare a ulozeni hodnot do SESSION
+if($idStranky == "playScene"){
+    for ($col = 1; $col < 4; $col++) {
+        for ($row = 1; $row < 4; $row++) {  
+            if (array_key_exists("row$row-col$col", $_GET)) {
+                $_SESSION["row$row-col$col"] = vyber($pocetKol,$znak1,$znak2);
+                header("Location: ?");
+                $_SESSION["pocetKol"]--;
+            } else {
+                $_SESSION["row$row-col$col"];
+            }
+        }
+    }
+}
+    
 
+
+// handle RESTARTU
+if(array_key_exists("restart",$_GET)){
+    session_destroy();
+    setcookie("PHPSESSID", "", time()-1, "/");
+    header("Location: ?");
+    exit;
+}
+
+//GAME LOGIC
+$rowWin=false;
+$colWin=false;
+$sikmoWin=false;
+//ROW WIN
+if($idStranky == "playScene"){
+    if ($_SESSION["row1-col1"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row1-col2"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row1-col3"] == vyber($pocetKol,$znak1,$znak2)){
+        $rowWin=true;
+    }else if ($_SESSION["row2-col1"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row2-col2"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row2-col3"] == vyber($pocetKol,$znak1,$znak2)){
+        $rowWin=true;
+    }else if ($_SESSION["row3-col1"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row3-col2"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row3-col3"] == vyber($pocetKol,$znak1,$znak2)){
+        $rowWin=true;
+    }
+    //COL WIN
+    if ($_SESSION["row1-col1"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row2-col1"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row3-col1"] == vyber($pocetKol,$znak1,$znak2)){
+        $colWin=true;
+    }else if ($_SESSION["row1-col2"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row2-col2"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row3-col2"] == vyber($pocetKol,$znak1,$znak2)){
+        $colWin=true;
+    }else if ($_SESSION["row1-col3"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row2-col3"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row3-col3"] == vyber($pocetKol,$znak1,$znak2)){
+        $colWin=true;
+    }
+    //sikmoWin
+    if ($_SESSION["row1-col1"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row2-col2"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row3-col3"] == vyber($pocetKol,$znak1,$znak2)){
+        $sikmoWin=true;
+    }else if ($_SESSION["row1-col3"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row2-col2"] == vyber($pocetKol,$znak1,$znak2) && $_SESSION["row3-col1"] == vyber($pocetKol,$znak1,$znak2)){
+        $sikmoWin=true;
+    }
+}
--- playScene.php
+++ playScene.php
@@ -3,61 +3,69 @@
         <div class="playContainer">
             <div class="box">
                 <div class="container" id="score">
-                    <h3>Score: </h3>
+                    <h3>Players</h3>
                     <hr>
-                    <h4>Games: </h4>
-                    <p>P1:<?php echo $_SESSION["hrac1"]?> </p>
-                    <p>P2:<?php echo $_SESSION["hrac2"]?> </p>
+                    <p><?php echo $_SESSION["hrac1"] ?> </p>
+                    <p><?php echo $_SESSION["hrac2"] ?> </p>
                 </div>
             </div>
             <div class="box">
                 <div class="container" id="play-field">
+                    <?php
+                    if ($rowWin || $colWin || $sikmoWin){
+                        echo "<div class='unclickable'>";
+                    }
+                    ?>
                     <table>
                         <?php
-                        for ($col = 1; $col < 4; $col++) {
+                        for ($row = 1; $row < 4; $row++) {
                             echo "<tr>";
-                            for ($row = 1; $row < 4; $row++) {
-                                echo "<td><a href=?row$row-col$col>";
-                                if (sudeKolo($pocetKol)) {
-                                    if (array_key_exists("row$row-col$col", $_GET)) {
-                                        echo $_SESSION["row$row-col$col"] = $znak1;
-                                    } else {
-                                        echo $_SESSION["row$row-col$col"];
-                                    }
-                                } else {
-                                    if (array_key_exists("row$row-col$col", $_GET)) {
-                                        echo $_SESSION["row$row-col$col"] = $znak2;
-                                    } else {
-                                        echo $_SESSION["row$row-col$col"];
-                                    }
+                            for ($col = 1; $col < 4; $col++) {
+                                echo "<td>";
+                                if ($_SESSION["row$row-col$col"] == ($znak1 || $znak2)){
+                                    echo "<div class='unclickable'>";
                                 }
-                                echo "</a></td>";
+                                echo "<a href=?row$row-col$col>";
+                                echo $_SESSION["row$row-col$col"];
+                                echo "</a>";
+                                if ($_SESSION["row$row-col$col"] == ($znak1 || $znak2)){
+                                    echo "</div>";
+                                }
+                                echo "</td>";
                             }
                             echo "</tr>";
                         }
+
                         ?>
+
                     </table>
+                    <?php
+                    if ($rowWin || $colWin || $sikmoWin){
+                        echo "</div>";
+                    }
+                    ?>
                 </div>
             </div>
             <div class="box">
                 <div class="container" id="player-turn">
-                    <h3><?php
-                        if (sudeKolo($pocetKol)) {
-                            echo $_SESSION["hrac1"];
-                        }else{
-                            echo $_SESSION["hrac2"];
-                        }
-                    ?>Turn</h3>
-                    
-                    <hr>
                     <?php
-                    if (sudeKolo($pocetKol)) {
-                        echo $znak2;
+                    if ($rowWin || $colWin || $sikmoWin){
+                        echo "<h3>Game Over</h3>";
+                        echo "<h4>";
+                        echo vyber($pocetKol,$_SESSION["hrac2"],$_SESSION["hrac1"]);
+                        echo " Wins</h4>";
+                        echo "<a href='?restart'><button class='button-30'>Restartovat</button></a>";
+                    }else if($pocetKol >8){
+                        echo "<h4>Remiza</h4>";
+                        echo "<a href='?restart'><button class='button-30'>Restartovat</button></a>";
                     }else{
-                        echo $znak1;
+                        echo "<h3>";
+                        echo vyber($pocetKol, $_SESSION["hrac1"], $_SESSION["hrac2"]);
+                        echo "'s Turn</h3>";
+                        echo vyber($pocetKol, $znak2, $znak1);
                     }
-                    
                     ?>
+                    
                 </div>
             </div>
 
